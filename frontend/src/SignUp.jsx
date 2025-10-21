364f7e94165092f14953214108b35f74d250564d
Adrian Pueblo <adrian.pueblo27@gmail.com>
2025-10-21 20:50:04 +0800

    `Update emailjs send function in SignUp.jsx to use environment variables`

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -22,12 +22,12 @@ function Signup() {
             //     alert(response.data.message);
 
             //     // Send welcome email after successful signup
-            //     await emailjs.send('service_szdov3u', 'template_hr5dj3i', { email }, 'pCvefZehxq1-eLPdP');
+            //     await emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, {email}, import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
             //     alert('Welcome email sent successfully!');
             // }
 
             // Send welcome email after successful signup
-            await emailjs.send('service_szdov3u', 'template_hr5dj3i', { email }, 'pCvefZehxq1-eLPdP');
+            await emailjs.send(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, {email}, import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
             alert('Welcome email sent successfully!');
         } catch (error) {
             if (error.response) {
